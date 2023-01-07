28799139c72c60cb4623c0b1abd59ace4dd92d38
Codigoquatro-Antony <codigoquatro2022@gmail.com>
2023-01-07 16:44:10 -0300

    created route product

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/produtos', function () {
+    return view('produto');
 });
+
+
